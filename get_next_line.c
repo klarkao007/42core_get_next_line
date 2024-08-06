de61dbb8bf0f7098388c606cf3e2ac362b42d407
Klara Bachova <kbachova@student.42prague.com>
2024-08-06 18:54:04 +0200

    skeleton of function added as comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,23 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:49 by kbachova          #+#    #+#             */
-/*   Updated: 2024/08/06 16:42:31 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/08/06 17:28:27 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <get_next_line.h>
+
 char	*get_next_line(int fd)
 {
+// check if fd not valid
 
+//read data from fd into buffer
+
+//store the leftover data in a static variable
+
+//find the newline char
+
+//create a string with the line and return it
+
+	return (NULL);
 }
