ea56e15ed0910c8176722b9b1f6858a6315b00d3
Klara Bachova <kbachova@student.42prague.com>
2024-08-09 14:21:53 +0200

    added some logic into gnl function (in progress)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:49 by kbachova          #+#    #+#             */
-/*   Updated: 2024/08/08 15:46:02 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/08/09 14:19:56 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 
 char	*get_next_line(int fd)
 {
-// check if fd not valid
+	static char	*storing_buffer;
+	char		*reading_buffer [BUFFER_SIZE + 1];
+	char		*line_buffer;
 
-//read data from fd into buffer
+	storing_buffer = NULL;
+	line_buffer = NULL;
 
-//store the leftover data in a static variable
-
-//find the newline char
-
-//create a string with the line and return it
-
+	if (!fd || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (fd)
+	{
+		read(fd, reading_buffer, BUFFER_SIZE);
+		if (!reading_buffer)
+			return (NULL);
+		strjoin(reading_buffer, storing_buffer);
+		if (find_newline(reading_buffer))
+		{
+			line_buffer = before_nl(storing_buffer);
+			storing_buffer = after_nl(storing_buffer);
+			return (line_buffer);
+		}
+		return (storing_buffer);
+	}
 	return (NULL);
 }
