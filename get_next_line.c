bd5d18dc115288644265e79e37c92d91e89ab51d
Klara Bachova <kbachova@student.42prague.com>
2024-08-09 16:35:20 +0200

    I tried to continue, added fctions to utils

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:49 by kbachova          #+#    #+#             */
-/*   Updated: 2024/08/09 14:19:56 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/08/09 15:45:47 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,27 @@ char	*get_next_line(int fd)
 	static char	*storing_buffer;
 	char		*reading_buffer [BUFFER_SIZE + 1];
 	char		*line_buffer;
+	int			bytes_read;
 
-	storing_buffer = NULL;
+	storing_buffer = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
 	line_buffer = NULL;
 
 	if (!fd || BUFFER_SIZE <= 0)
 		return (NULL);
-	while (fd)
+	while (bytes_read > 0)
 	{
-		read(fd, reading_buffer, BUFFER_SIZE);
-		if (!reading_buffer)
+		bytes_read = 0;
+		bytes_read = read(fd, reading_buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
 			return (NULL);
-		strjoin(reading_buffer, storing_buffer);
+		reading_buffer[bytes_read] = '\0';
 		if (find_newline(reading_buffer))
 		{
 			line_buffer = before_nl(storing_buffer);
 			storing_buffer = after_nl(storing_buffer);
 			return (line_buffer);
 		}
-		return (storing_buffer);
+		ft_strjoin(reading_buffer, storing_buffer);
 	}
 	return (NULL);
 }
