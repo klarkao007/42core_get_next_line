d94d92f1239bddd8cd6ce0d1a8bfec9ee11f0b48
Klara Bachova <kbachova@student.42prague.com>
2024-08-22 15:34:16 +0200

    added alternative logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:49 by kbachova          #+#    #+#             */
-/*   Updated: 2024/08/12 17:02:53 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/08/22 15:34:08 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,17 +44,27 @@ char	*get_next_line(int fd)
 
 // FUNCTION LOGIC:
 // char	*get_next_line(int fd)
-
+//
 // 	static char	*storage;
 // 	char	*line;
-
+//
 // 	while (read(fd, buf, nbytes) > 0)
-// 	(copies nbytes to buf, moves the "cursor", returns n of bytes read (-1 error, 0 end of file))
+//
+// 	(copies nbytes to buf, moves the "cursor",
+//	returns n of bytes read (-1 error, 0 end of file))
+//
 // 	strjoin buf to storage
+//
 // 	if storage has \n
 // 		copy all from storage up to \n to line
-// 		delete all from storage up to \n
+// 		modify storage to only contain chars after \n
 // 		return line
-// 	else = read again
+//
+//	ALTERNATIVE (needs to add copy storage to line first and empty storage)
+// 	if line has \n
+// 		copy all from line after \n to storage
+// 		delete all from line after \n
+// 		return line
+//
+// 	(else = read again)
 // 	return NULL
-
