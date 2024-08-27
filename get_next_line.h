804a8c66428e9ddf29176f242c3ad7d5cc681fdd
Klara Bachova <kbachova@student.42prague.com>
2024-08-27 15:20:08 +0200

    fixed header inclusion of buffer size

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:46 by kbachova          #+#    #+#             */
-/*   Updated: 2024/08/08 15:51:34 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/08/27 15:19:44 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@
 # include <fcntl.h>
 # include <stdio.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 char	*get_next_line(int fd);
 
 #endif
-
-// !Find a way to include this in the above definition!
-// #ifndef BUFFER_SIZE
-// # define BUFFER_SIZE 10
-// #endif
