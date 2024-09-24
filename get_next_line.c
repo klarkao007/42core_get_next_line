e9782718cfc4196bca3080947665c496a0fa82ab
Klara Bachova <kbachova@student.42prague.com>
2024-09-24 14:54:11 +0200

    deleted unnecessary check and free in set line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbachova <kbachova@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 16:19:49 by kbachova          #+#    #+#             */
-/*   Updated: 2024/09/24 14:48:55 by kbachova         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:53:23 by kbachova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,11 +98,6 @@ char	*set_line(char *line)
 	if (line[i] == '\0' || line[i + 1] == '\0')
 		return (NULL);
 	storage = ft_substr(line, i + 1, ft_strlen(line) - i);
-	if (*storage == '\0')
-	{
-		free(storage);
-		storage = NULL;
-	}
 	line[i + 1] = '\0';
 	return (storage);
 }
